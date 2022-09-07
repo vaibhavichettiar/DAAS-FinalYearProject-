8aec00237e5f6602e0a584596b7ec29017f88ee2
Nishanth09 <inumpudinishanth96@gmail.com>
2022-09-07 16:24:04 -0700

    redux added and routes implemented

---
+++ client/src/components/Landing/LandingComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LandingComponent() {
+    console.log("here");
+    return ( 
+    <div>
+        <h1>Landing page</h1>
+    </div> );
+}
+
+export default LandingComponent;
--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
 
-app.get('/',(req,res)=>res.send('hello world'));
-app.listen(5000,()=>{
-    console.log('my server is running on port 5000');
+app.get('/',(req,res)=>res.send('hello world!!!!!!!'));
+app.listen(5001,()=>{
+    console.log('my server is running on port 5001');
 })
