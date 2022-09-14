6b789b58d39fc7b1f8ea0f7c41fea6b7d4d5c27e
Nishanth09 <inumpudinishanth96@gmail.com>
2022-09-13 18:50:43 -0700

    kafka added

--- server/app.js
+++ server/app.js
@@ -3,7 +3,10 @@ const app = express();
 var kafka_consumer = require('./kafka/consumer');
 var kafka = require('kafka-node');
 var config = require('./config/config.json')
+const cors = require('cors');
 
+app.use(express.json());
+app.use(cors());
 app.get('/',(req,res) => {
     res.sendStatus(200);
     Producer = kafka.Producer,
@@ -25,6 +28,10 @@ app.get('/',(req,res) => {
         client.close();
     });
 });
+app.post('/auth/login', (req, res) => {
+    console.log(req.body);
+    res.sendStatus(200);
+})
 app.listen(5001,()=>{
     console.log('my server is running on port 5001');
     kafka_consumer.consumer();
