2d1cd25d93bb95c738c477e92291df4707431a2f
vishwashah7e7 <vishwashah7e7@gmail.com>
2022-09-20 15:35:45 -0700

    Prediction using Prophet model

--- processingServer/app/models/cassandra.py
+++ processingServer/app/models/cassandra.py
@@ -14,19 +14,39 @@ class cassandraConnection:
     @staticmethod
     def initCassandraConnection():
         global cassConn
-        if(cassConn == None):
-            try :
-                node_ips = [os.environ['CASSANDRA_HOST']]
-                auth_provider = PlainTextAuthProvider(username='cassandra', password= os.environ['CASSANDRA_PASSWORD'])
-                cluster = Cluster(node_ips, port = 9042, protocol_version=4, auth_provider=auth_provider)
-                cassConn = cluster.connect()
-                cassConn.execute(""" CREATE KEYSPACE IF NOT EXISTS %s WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };
-                """ % KEYSPACE)
-                cassConn = cluster.connect(keyspace=KEYSPACE)
-            except:
-                logger.error("Not able to connect to cassandra server")
+        flag = True
+        while flag:
+            if(cassConn == None):
+                try :
+                    node_ips = [os.environ['CASSANDRA_HOST']]
+                    auth_provider = PlainTextAuthProvider(username='cassandra', password= os.environ['CASSANDRA_PASSWORD'])
+                    cluster = Cluster(node_ips, port = 9042, protocol_version=4, auth_provider=auth_provider)
+                    cassConn = cluster.connect()
+                    cassConn.execute(""" CREATE KEYSPACE IF NOT EXISTS %s WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };
+                    """ % KEYSPACE)
+                    cassConn = cluster.connect(keyspace=KEYSPACE)
+                    cassandraConnection.createTable(cassandraConnection.generateProcessingTableQuery())
+                    cassandraConnection.createTable(cassandraConnection.generateModelingTableQuery())
+                    flag = False
+                except Exception as Argument:
+                    logger.error("Not able to connect to cassandra server. Reason: %s", Argument)
 
         
         return cassConn
 
+    @staticmethod
+    def generateProcessingTableQuery():
+        return "CREATE TABLE IF NOT EXISTS " + KEYSPACE + ".processIds(id UUID PRIMARY KEY, tablename text);"
+
+    @staticmethod
+    def generateModelingTableQuery():
+        return "CREATE TABLE IF NOT EXISTS " + KEYSPACE + ".models(id UUID, productid int, filename text, PRIMARY KEY (id, productid));"
+
+    @staticmethod
+    def createTable(createTableQuery):
+        try:
+            cassConn.execute(createTableQuery)
+            logger.info("Table created suceessfully for query: %s" , createTableQuery)
+        except:
+            logger.error("Unable to create table: %s", createTableQuery)
 
