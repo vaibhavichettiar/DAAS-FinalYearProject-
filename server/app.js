844a98dc8d8d8272db4c5475b7d80794c0e442fb
karthik kashyap <kashyapkarthik3@gmail.com>
2022-10-11 13:11:29 -0700

    Add Amazon Keyspace

--- server/app.js
+++ server/app.js
@@ -1,44 +1,26 @@
 const express = require('express');
 const app = express();
-var kafka_consumer = require('./kafka/consumer');
 var kafka = require('kafka-node');
 var config = require('./config/config.json');
 const cors = require('cors');
-const cassandra = require('cassandra-driver');
 const route = require('./routes/routes.js');
+const { client } = require("./config/config");
 
 const hostname = config.cassandra.host;
 app.use(express.json())
-// const client = new cassandra.Client({ 
-//     contactPoints: [hostname],
-//     localDataCenter: 'datacenter1',
-//     credentials: { username: 'cassandra', password: 'cassandra' }
-// });
-
-// client.connect(function (err) {
-//     console.log(err);
-// });
-
-// const query = "SELECT * FROM daas_ks.users";
-
-// client.execute(query, function (err, result) {
-//   var user = result.first();
-//   //The row is an Object with column names as property keys. 
-//   console.log('My name is %s and my id is %s', user.name);
-// });
 
 app.use(cors());
 app.use('/api', route);
-//app.use('/file');
-app.get('/',(req,res) => {
+
+app.get('/', (req, res) => {
     res.sendStatus(200);
     Producer = kafka.Producer,
-    KeyedMessage = kafka.KeyedMessage,
-    connectionString = config.kafka.host,
-    client = new kafka.KafkaClient(connectionString),
-    producer = new Producer(client);
+        KeyedMessage = kafka.KeyedMessage,
+        connectionString = config.kafka.host,
+        client = new kafka.KafkaClient(connectionString),
+        producer = new Producer(client);
     payloads = [
-        { topic: 'test_topic', messages: "Hello....."}
+        { topic: 'test_topic', messages: "Hello....." }
     ];
     producer.on('ready', function () {
         producer.send(payloads, function (err, data) {
@@ -52,7 +34,6 @@ app.get('/',(req,res) => {
     });
 });
 
-app.listen(5001,()=>{
+app.listen(5001, () => {
     console.log("my server is running on port 5001");
-    //kafka_consumer.consumer();
 })
